cf7d081d08700a4411bbdc8b6df174053a07516f
Ольга <141999010+olga1234lanina@users.noreply.github.com>
2023-09-12 22:03:45 +0500

    Update Program.cs
    решение

--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿﻿// pадача 38. задайте массив вещественных чисел. найдите разницу между максимальным и минимальным элементов массива.
+// [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+
+
+Console.Clear();
+int[] array = GetArray();
+PrintArray(array);
+Console.Write($" -> {GetDiff(array)}");
+
+int[] GetArray()
+{
+  int size = new Random().Next(5, 10);
+  int[] array = new int[size];
+  for (int i = 0; i < array.Length; i++)
+  {
+    array[i] = new Random().Next(0, 100);
+  }
+  return array;
+}
+
+void PrintArray(int[] array)
+{
+  Console.Write($"[{String.Join(", ", array)}]");
+}
+
+int GetDiff(int[] array)
+{
+  int min = 100, max = 0, result = 0;
+  foreach (int el in array)
+  {
+    if (max < el) max = el;
+    if (min > el) min = el;
+  }
+  result = max - min;
+  return result;
+}
